65ca39c1282de2dcc4cbed0018a0d3ab8a44b2f0
SteveMcArthur <contact@stevemcarthur.co.uk>
2015-03-28 12:03:51 +0000

    Changed the base language to htmlembedded with the scripting ModeSpec "coffeescript". Added modified version of coffeescript codemirror language mode to handle things specific to eco - in particular comments. Test eco files to test functioning of highlighter

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
+/*global define, brackets */
 
 /** Simple extension that tells brackets how to recognise eco templates and treat them the same as ejs templates*/
 define(function (require, exports, module) {
-	"use strict";
+    "use strict";
 
-	var LanguageManager = brackets.getModule("language/LanguageManager");
+    require('./coffeescript2');
 
-	var language = LanguageManager.getLanguage("ejs");
+    var codemirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+    codemirror.defineMIME("application/x-eco", {name: "htmlembedded", scriptingModeSpec:"coffeescript2"});
+    var LanguageManager = brackets.getModule("language/LanguageManager");
 
-	language.addFileExtension("eco");
-	language.addFileName("ECO");
-	language.setBlockCommentSyntax("<!--", "-->");
+    LanguageManager.defineLanguage("eco", {
+        name: "ECO",
+        mode: ["htmlembedded", "application/x-eco"],
+        fileExtensions: ["eco","html.eco"]
+    });
+
+
+    var language = LanguageManager.getLanguage("coffeescript2");
+
+    console.log("lang= "+language.getName());
+
+//    var language = LanguageManager.getLanguage("ejs");
+//
+//    language.addFileExtension("eco");
+//    language.addFileName("ECO");
+//    language.setLineCommentSyntax("<%#", "%>");
+//    language.setBlockCommentSyntax("<%##", "%>");
 });
